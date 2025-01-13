b8f820355cffe88376d753ccef93c5494ae1cb6f
gakhoka <giorgigakhokidze14@gmail.com>
2025-01-13 20:22:36 +0100

    milk and syrup selection views completed

---
+++ MagicCoffee/MagicCoffee/CoffeeAssemblagePage/MilkTypeView.swift
@@ -0,0 +1,65 @@
+//
+//  OptionSelectionView.swift
+//  MagicCoffee
+//
+//  Created by Giorgi Gakhokidze on 13.01.25.
+//
+
+import SwiftUI
+
+struct OptionSelectionView: View {
+    
+    @Environment(\.dismiss) var dismiss
+    @Binding var selectedOption: String
+    
+    let title: String
+    let options: [String]
+    
+    var body: some View {
+        VStack(spacing: 0) {
+            
+            Text(title)
+                .foregroundColor(.gray)
+                .padding(.top)
+            
+            VStack(spacing: 5) {
+                ForEach(options, id: \.self) { option in
+                    Button(action: {
+                        selectedOption = option
+                        dismiss()
+                    }) {
+                        Text(option)
+                            .frame(maxWidth: .infinity)
+                            .frame(height: 50)
+                            .foregroundColor(.black)
+                    }
+                    
+                    Divider()
+                }
+            }
+            .background(Color.clear)
+            .cornerRadius(12)
+            .padding(.horizontal)
+            
+            cancelButton
+        }
+        .poppinsFont(size: 16)
+        .presentationDetents([
+            .height(400)
+        ])
+    }
+    
+    private var cancelButton: some View {
+        Button(action: {
+            dismiss()
+        }) {
+            Text("Cancel")
+                .frame(maxWidth: .infinity)
+                .frame(height: 50)
+                .foregroundStyle(.black)
+                .font(.system(size: 16, weight: .heavy))
+        }
+        .padding()
+    }
+}
+
