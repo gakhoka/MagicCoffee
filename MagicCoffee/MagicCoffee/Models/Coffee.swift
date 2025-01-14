78cc872d792b6ac8d75f922c2428fbb6f0307adb
gakhoka <giorgigakhokidze14@gmail.com>
2025-01-14 19:12:40 +0100

    models created

---
+++ MagicCoffee/MagicCoffee/Models/Coffee.swift
@@ -0,0 +1,39 @@
+//
+//  Coffee.swift
+//  MagicCoffee
+//
+//  Created by Giorgi Gakhokidze on 14.01.25.
+//
+
+import Foundation
+
+
+struct Coffee: Identifiable {
+    let id = UUID()
+    let name: String
+    let ristreto: Int
+    let size: CoffeeSize
+    let image: String
+    let type: CoffeeType
+    let sort: [Country]
+    let grinding: GrindingLevel
+    let milk: [String]?
+    let syrup: [String]?
+    let iceAmount: Int
+    let roastingLevel: Int
+    let additives: [String]?
+    let score: Int
+    let redeemPointsAmount: Int
+    
+    
+    enum CoffeeSize: String {
+        case small, medium, large
+    }
+
+    enum GrindingLevel: Int {
+        case fine = 0, medium
+    }
+}
+
+
+
