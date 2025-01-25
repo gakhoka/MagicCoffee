a51762428c54e7bad00174cb1428dc5aa43e4fa8
gakhoka <giorgigakhokidze14@gmail.com>
2025-01-25 16:05:59 +0100

    add cards view

---
+++ MagicCoffee/MagicCoffee/PaymentPage/CardDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  CardDetailsView.swift
+//  MagicCoffee
+//
+//  Created by Giorgi Gakhokidze on 25.01.25.
+//
+
+import SwiftUI
+
+struct CardDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CardDetailsView()
+}
