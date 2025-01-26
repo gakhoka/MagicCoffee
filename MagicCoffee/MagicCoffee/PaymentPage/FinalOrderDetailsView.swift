710a01c383e9530593cd741de90f97d2e8926be7
gakhoka <giorgigakhokidze14@gmail.com>
2025-01-26 08:44:02 +0100

    successful order view

---
+++ MagicCoffee/MagicCoffee/PaymentPage/FinalOrderDetailsView.swift
@@ -0,0 +1,18 @@
+//
+//  FinalOrderDetailsView.swift
+//  MagicCoffee
+//
+//  Created by Giorgi Gakhokidze on 25.01.25.
+//
+
+import SwiftUI
+
+struct FinalOrderDetailsView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    FinalOrderDetailsView()
+}
